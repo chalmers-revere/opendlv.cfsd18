37e140d8cac9fa9c2d228e02ea38384f3a032c8b
somliga <Somliga@gmail.com>
2017-11-22 12:04:46 +0100

    Merged lidar object detection with fusion, added communication between modules in fusion microprocess

--- code/logic-cfsd18/perception/detectcone/include/detectcone.hpp
+++ code/logic-cfsd18/perception/detectcone/include/detectcone.hpp
@@ -20,16 +20,31 @@
 #ifndef OPENDLV_LOGIC_CFSD18_PERCEPTION_DETECTCONE_HPP
 #define OPENDLV_LOGIC_CFSD18_PERCEPTION_DETECTCONE_HPP
 
-#include <opendavinci/odcore/base/module/DataTriggeredConferenceClientModule.h>
-#include <opendavinci/odcore/data/Container.h>
+#include <memory>
+#include <vector>
+#include <cmath>
 
-//#include <odvdopendlvstandardmessageset/GeneratedHeaders_ODVDOpenDLVStandardMessageSet.h>
+#include "opendavinci/odcore/base/module/DataTriggeredConferenceClientModule.h"
+#include "opendavinci/odcore/data/Container.h"
 #include <odvdcfsd18/GeneratedHeaders_ODVDcfsd18.h>
+
+#include "opendavinci/generated/odcore/data/CompactPointCloud.h"
+ 
+#include <opendavinci/odcore/wrapper/Eigen.h>
+
+#include <opendavinci/odcore/data/TimeStamp.h>
+
 
 namespace opendlv {
 namespace logic {
 namespace cfsd18 {
 namespace perception {
+
+using namespace std;
+using namespace Eigen;
+using namespace odcore::base;
+using namespace odcore::data;
+using namespace odcore::wrapper;
 
 class DetectCone : public odcore::base::module::DataTriggeredConferenceClientModule {
  public:
@@ -40,8 +55,26 @@ class DetectCone : public odcore::base::module::DataTriggeredConferenceClientMod
   virtual void nextContainer(odcore::data::Container &);
 
  private:
+  void matchPoints(MatrixXd, MatrixXd);
+  void findMatch(MatrixXd, MatrixXd);
+  MatrixXd Spherical2Cartesian(double, double, double);
+  opendlv::logic::sensation::Point Cartesian2Spherical(double, double, double);
+  void SendCollectedCones(MatrixXd);
+  void SendMatchedContainer(MatrixXd);
   void setUp();
   void tearDown();
+  MatrixXd m_lastLidarData;
+  MatrixXd m_lastCameraData;
+  MatrixXd m_pointMatched;
+  double m_diffVec;
+  MatrixXd m_finalPointCloud;
+  double m_threshold;
+  int64_t m_timeDiffMilliseconds;
+  odcore::data::TimeStamp m_lastTimeStamp;
+  MatrixXd m_coneCollector;
+  int m_coneCollected;
+  const double DEG2RAD = 0.017453292522222; // PI/180.0
+  const double RAD2DEG = 57.295779513082325; // 1.0 / DEG2RAD;
 };
 
 }
