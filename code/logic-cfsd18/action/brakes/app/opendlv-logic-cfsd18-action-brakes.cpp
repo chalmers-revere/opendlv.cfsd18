2f8e97617cfdbc399d27bcda28eb0c4984964b93
Meetra13 <meet@student.chalmers.se>
2018-01-30 15:30:46 +0100

    added folder "brakes"

--- code/logic-cfsd18/action/brakes/app/opendlv-logic-cfsd18-action-brakes.cpp
+++ code/logic-cfsd18/action/brakes/app/opendlv-logic-cfsd18-action-brakes.cpp
@@ -16,27 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef OPENDLV_LOGIC_CFSD18_ACTION_LONGITUDINAL_TESTSUITE_HPP
-#define OPENDLV_LOGIC_CFSD18_ACTION_LONGITUDINAL_TESTSUITE_HPP
+#include "brakes.hpp"
 
-#include "cxxtest/TestSuite.h"
-
-#include "../include/longitudinal.hpp"
-
-class LongitudinalTest : public CxxTest::TestSuite {
-  public:
-    void setUp()
-    {
-    }
-
-    void tearDown()
-    {
-    }
-
-    void testApplication()
-    {
-      TS_ASSERT(true);
-    }
-};
-
-#endif
+int32_t main(int32_t a_argc, char **a_argv) {
+  opendlv::logic::cfsd18::action::Brakes app(a_argc, a_argv);
+  return app.runModule();
+}
