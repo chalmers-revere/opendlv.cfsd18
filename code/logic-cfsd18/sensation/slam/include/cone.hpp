b405dc9115d41310354bc5061eb8ebf0470e3964
martin <martin9421@gmail.com>
2018-02-28 14:02:42 +0100

    added new non-functional cone class

---
+++ code/logic-cfsd18/sensation/slam/include/cone.hpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2017 Chalmers Revere
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+#ifndef OPENDLV_LOGIC_CFSD18_SENSATION_CONE_HPP
+#define OPENDLV_LOGIC_CFSD18_SENSATION_CONE_HPP
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+
+namespace opendlv {
+namespace logic {
+namespace cfsd18 {
+namespace sensation {
+
+class Cone{
+  public:
+    Cone(double x, double y,std::string property,int id);
+    ~Cone();
+    
+    double getX();
+    double getY();
+    std::string getProperty();
+    int getId();
+    
+    void setX(double x);
+    void setY(double y);
+    void setProperty(std::string property);
+    void setId(int id);
+
+    //g2o::pointxy_vertex coneToVertex();
+    //void vertexToCone(vertex);
+  private:
+    double m_x;
+    double m_y;
+    std::string m_property;
+    int m_id;
+
+};
+
+} //sensation
+} //cfsd18
+} //logic
+} //opendlv
+
+#endif
