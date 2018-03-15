3dddd9d0453dc43a05a724c8ef02d05b072ec54b
martin <martin9421@gmail.com>
2018-03-15 11:19:19 +0100

    Added sender stamps

--- code/logic-cfsd18/sensation/geolocator/include/geolocator.hpp
+++ code/logic-cfsd18/sensation/geolocator/include/geolocator.hpp
@@ -74,6 +74,8 @@ class Geolocator : public odcore::base::module::TimeTriggeredConferenceClientMod
   double m_headingReading;
   double m_groundSpeedReading;
   MatrixXd m_states;
+  uint32_t m_senderStamp = 114;
+  uint32_t m_imuSenderStamp = 112;
   //odcore::base::Mutex m_egoStateMutex;
 
   //void nextContainer(odcore::data::Container &);
