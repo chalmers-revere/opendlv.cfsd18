54fcef593f1546c40835869d16aba91be95b8301
linarn <linus.arno@gmail.com>
2018-04-04 17:44:35 +0200

    Minor changes

--- code/logic-cfsd18/cognition/track/src/track.cpp
+++ code/logic-cfsd18/cognition/track/src/track.cpp
@@ -339,9 +339,9 @@ std::vector<float> Track::curvatureTriCircle(Eigen::MatrixXf localPath, int step
 }
 
 std::vector<float> Track::curvaturePolyFit(Eigen::MatrixXf localPath){ // TODO: double check coordinate system, maybe switch x/y
-  int polynomialDegree = 3; //TODO: add as config
+  int n = 3; //polynomial degree TODO: add as config
   int pointsPerSegment = 15; //TODO: add as config
-  int i,j,n,segments,N;
+  int i,j,segments,N;
   int k=0;
   uint32_t l=0;
   Eigen::VectorXf dividedPathX(localPath.rows()); // TODO: This is now maximum possible size, which in some cases is unneccesary
@@ -380,7 +380,7 @@ std::vector<float> Track::curvaturePolyFit(Eigen::MatrixXf localPath){ // TODO: 
   Eigen::VectorXf pathy;
   Eigen::VectorXf x;
   Eigen::VectorXf y;
-  Eigen::VectorXf a(polynomialDegree+1);
+  Eigen::VectorXf a(n+1);
   int segmentBegin;
   int segmentLength;
 
@@ -394,7 +394,6 @@ std::vector<float> Track::curvaturePolyFit(Eigen::MatrixXf localPath){ // TODO: 
     segments = floor(pathx.size()/N); //number of segments
 
     for (int p=0; p<segments; p++){
-      n = polynomialDegree;
       if ((p<segments-1) || (!(segments*N<pathx.size())) ){
       segmentBegin = p*N;
       segmentLength = N;
@@ -426,28 +425,25 @@ std::vector<float> Track::curvaturePolyFit(Eigen::MatrixXf localPath){ // TODO: 
       }
       for (i=0;i<=n;i++)
         B(i,n+1)=Y(i);                //load the values of Y as the last column of B(Normal Matrix but augmented)
-      n=n+1;                //n is made n+1 because the Gaussian Elimination part below was for n equations, but here n is the degree of polynomial and for n degree we get n+1 equations
 
-      for (i=0;i<n;i++)                    //From now Gaussian Elimination starts(can be ignored) to solve the set of linear equations (Pivotisation)
-        for (k=i+1;k<n;k++)
+      for (i=0;i<n+1;i++)                    //From now Gaussian Elimination starts(can be ignored) to solve the set of linear equations (Pivotisation)
+        for (k=i+1;k<n+1;k++)
             if (B(i,i)<B(k,i))
-                for (j=0;j<=n;j++){
+                for (j=0;j<=n+1;j++){
                     float temp=B(i,j);
                     B(i,j)=B(k,j);
                     B(k,j)=temp;
                 }
-
-      for (i=0;i<n-1;i++)            //loop to perform the gauss elimination
-        for (k=i+1;k<n;k++){
+      for (i=0;i<n;i++)            //loop to perform the gauss elimination
+        for (k=i+1;k<n+1;k++){
                 float t=B(k,i)/B(i,i);
-                for (j=0;j<=n;j++)
+                for (j=0;j<=n+1;j++)
                     B(k,j)=B(k,j)-t*B(i,j);    //make the elements below the pivot elements equal to zero or elimnate the variables
             }
-      for (i=n-1;i>=0;i--){                //back-substitution
-                                    //x is an array whose values correspond to the values of x,y,z..
-        a(i)=B(i,n);                //make the variable to be calculated equal to the rhs of the last equation
-        for (j=0;j<n;j++)
-            if (j!=i)            //then subtract all the lhs values except the coefficient of the variable whose value                                   is being calculated
+      for (i=n;i>=0;i--){                //back-substitution
+        a(i)=B(i,n+1);                //make the variable to be calculated equal to the rhs of the last equation
+        for (j=0;j<n+1;j++)
+            if (j!=i)            //then subtract all the lhs values except the coefficient of the variable whose value is being calculated
                 a(i)=a(i)-B(i,j)*a(j);
         a(i)=a(i)/B(i,i);            //now finally divide the rhs by the coefficient of the variable to be calculated
       }
