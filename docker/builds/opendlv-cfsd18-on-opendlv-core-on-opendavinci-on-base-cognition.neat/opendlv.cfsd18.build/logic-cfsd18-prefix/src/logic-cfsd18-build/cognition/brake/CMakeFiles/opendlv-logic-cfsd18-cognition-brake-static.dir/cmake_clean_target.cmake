18dd88e2f02b9d7ece4d4f2f8ebd90d39d1c29d7
somliga <somliga@gmail.com>
2018-05-21 16:59:44 +0200

    Remove unneccesary folders

--- docker/builds/opendlv-cfsd18-on-opendlv-core-on-opendavinci-on-base-cognition.neat/opendlv.cfsd18.build/logic-cfsd18-prefix/src/logic-cfsd18-build/cognition/brake/CMakeFiles/opendlv-logic-cfsd18-cognition-brake-static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libopendlv-logic-cfsd18-cognition-brake-static.a"
-)
--- docker/builds/opendlv-cfsd18-on-opendlv-core-on-opendavinci-on-base-cognition.neat/opendlv.cfsd18.build/logic-cfsd18-prefix/src/logic-cfsd18-build/cognition/limitlateral/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/opendlv.cfsd18.sources/code/logic-cfsd18/cognition/limitlateral
-# Build directory: /opt/opendlv.cfsd18.build/logic-cfsd18-prefix/src/logic-cfsd18-build/cognition/limitlateral
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(limitlateraltestsuite.hpp-TestSuite "/opt/opendlv.cfsd18.build/logic-cfsd18-prefix/src/logic-cfsd18-build/cognition/limitlateral/limitlateraltestsuite.hpp-TestSuite")
-set_tests_properties(limitlateraltestsuite.hpp-TestSuite PROPERTIES  TIMEOUT "3000")
